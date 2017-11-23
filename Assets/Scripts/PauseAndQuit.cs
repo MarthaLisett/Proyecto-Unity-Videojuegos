cbf11f8fbc401a466d81e47bdae1f141f0ff4599
eGuajardoMozo <estefania_gm13@hotmail.com>
2017-11-23 17:41:12 -0600

    Menu scenes & pause and quit

---
+++ Assets/Scripts/PauseAndQuit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseAndQuit : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+            if(Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+}
